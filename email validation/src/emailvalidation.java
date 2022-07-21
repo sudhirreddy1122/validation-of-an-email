801746a74db95d499f038cd3dadb67f72baf1cf5
sudhirreddy1122 <sudhir17072000@gmail.com>
2022-07-21 19:53:15 +0530

    email validation

---
+++ email validation/src/emailvalidation.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class emailvalidation {
+
+    public static void main(String[] args) {
+        
+        ArrayList<String> emailID = new ArrayList<String>();
+        
+        
+        emailID.add("hhvm@movie.com");
+        emailID.add("rc15@movie.com");
+        emailID.add("salar@movie.com");
+        emailID.add("ntr30@movie.com");
+        emailID.add("pushpa2@movie.com");
+        emailID.add("mb28@movie.com");
+       
+        int c = 0;
+        
+   
+        String searchEmail = "rc15@movie.com";
+        
+                for(int i=0; i<emailID.size(); i++) {
+                    
+                    
+                    if(searchEmail==emailID.get(i)) {
+                        
+                        System.out.println("\n");
+                        
+                       c = 1;
+                        
+                        break;
+                        
+                    }
+                }
+                
+                if(c==1)
+                {System.out.println("email ID :" + searchEmail + "  is found");}
+                else{System.out.println("email ID Not found");}
+
+    }
+
+}
